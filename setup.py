af88feb488434223292715b5ef967fe5761e522d
dmaxson <dmaxson@ltsnet.net>
2018-02-16 11:03:28 -0500

    Initial commit (with files)

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='easyrabbit',
+    version='0.0.0',
+    packages=['easyrabbit'],
+    url='https://github.com/scnerd/easyrabbit',
+    license='MIT',
+    author='scnerd',
+    author_email='',
+    description=''
+)
