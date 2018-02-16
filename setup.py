199c146e45dbb9a5e444b321d223fc0f64656705
dmaxson <dmaxson@ltsnet.net>
2018-02-16 15:00:07 -0500

    Documentation changes

--- setup.py
+++ setup.py
@@ -2,12 +2,13 @@ from setuptools import setup
 
 setup(
     name='easyrabbit',
-    version='0.0.0',
+    version='0.0.2',
     packages=['easyrabbit'],
     url='https://github.com/scnerd/easyrabbit',
     license='MIT',
     author='scnerd',
     author_email='',
+    long_description=open('README.rst').read(),
     description='Easy utilities for common RabbitMQ tasks',
     install_requires=[
         'pika>=0.10',
